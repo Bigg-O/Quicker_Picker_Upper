1fb15523cf86ff288b19aa2e60748a1aae113b70
Joellehelm <joellenhelm@gmail.com>
2019-10-22 16:05:03 -0500

    start of layouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,14 @@ class SessionsController < ApplicationController
         
     end
     def create
-        @user = User.find_by_username(params[:username])
+        @user = User.find_by(user_name: user_params[:user_name])
+        puts user_params
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to users_path notice: "Logged in!"
         else
             # flash.now[:alert] = "Email or password is invalid"
+            puts "ooh nooopppppppooooooooooooooooo"
             render :new
         end
     end
@@ -23,7 +25,7 @@ class SessionsController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:user_name, :password)
     end
     
 end
