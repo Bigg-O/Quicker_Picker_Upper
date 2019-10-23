7d63c2a7479fdde75ec350f52a99c704dd350f8f
swdecker <swdecker@gmail.com>
2019-10-23 14:06:49 -0500

    added methods to rooms and room controller to update the game every time the user does a game action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-
+    @@timeStep = 0
     def newgame
     end
     
@@ -26,8 +26,36 @@ class RoomsController < ApplicationController
         @gamestat = current_user.gamestats.last
 
     end
+    
 
+    private
 
+    def timecheck
+        # checks the time and returns an integer that is the amount of elapsed 30 second periods
+        # stores the current time step in a variable
+       ((Time.now - @@playTime)/30).to_i
+    end
+    def gameover?
+        if Room.gameover?
+            redirect_to gameover_path
+        end
+    end
+    def update
+        # finds the current step (a step is a 30 second chunk of time)
+        # adds kids and messes for each step that has passed
+        # redirects to gameover if you add a 5th mess to a room
+        # updates the timestep of the controller
+        current_step = timecheck
+        if current_step > @@timeStep
+            (current_step - @@timeStep).times do
+                Room.addKids
+                if !Room.addmesses
+                    redirect_to gameover_path
+                end
+            end
+            @@timeStep = current_step
+        end
+    end
 
 
 
