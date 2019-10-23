afd0bba186401340bc982652ea7c54b1ff8543cd
Wookeun Song <wookeun.s@gmail.com>
2019-10-23 10:17:13 -0500

    Shows inventory

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,7 @@
 class RoomsController < ApplicationController
 
+
+    
     def index
         @rooms = Room.all
     end
--- app/models/gamestat.rb
+++ app/models/gamestat.rb
@@ -1,8 +1,10 @@
 class Gamestat < ApplicationRecord
     belongs_to :user
+
     def self.top_five_messes
         Gamestat.all.max_by(5){ |gamestat| gamestat.messes_cleaned }
     end
+    
     def self.longest_games
         Gamestat.all.max_by(5){ |gamestat| gamestat.elapsed_time }
     end
